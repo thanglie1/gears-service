228b45f852821f2b242f563fa65141a8ac05d329
Huan Le <huan.le@opswat.com>
2019-10-12 11:10:29 +0700

    huan commit

--- GearsContract.h
+++ GearsContract.h
@@ -1,14 +1,13 @@
 #pragma once
-#include <maf/messaging/client-server/CSContractBegin.mc.h>
-#include <maf/messaging/client-server/CSContractDefines.mc.h>
+#include <maf/messaging/client-server/CSContractDefinesBegin.mc.h>
 
 #define SERVER_DOMAIN_NAME "com.opswat.agent"
 #define SERVICE_ID_SCANNING_SERVICE 1
 
 
-result_object_s(ScanningStatus)
-	properties((int, percentage))
-result_object_e(ScanningStatus)
+function(ScanningStatus)
+        has_result((int, percentage))
+        request_noparams()
+endfunc()
 
-
-#include <maf/messaging/client-server/CSContractEnd.mc.h>
+#include <maf/messaging/client-server/CSContractDefinesEnd.mc.h>
--- Main.cpp
+++ Main.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
 #include <maf/messaging/client-server/ipc/LocalIPCServiceStub.h>
 #include <maf/messaging/client-server/ipc/LocalIPCServer.h>
-#include <maf/messaging/Component.h>
+#include <maf/messaging/ExtensibleComponent.h>
 #include <maf/messaging/Timer.h>
 #include "GearsContract.h"
 
 using namespace maf::messaging;
 
-class ScanServiceComp : public Component
+class ScanServiceComp : public ExtensibleComponent
 {
 public:
 	maf::messaging::Timer _timer;
 	int _percentage = 0;
 	std::shared_ptr<ipc::LocalIPCServiceStub> _serviceStub;
 
-	void start()
+	void onEntry() override
 	{
-		this->_detached = false;
 		onMessage<ipc::IPCClientRequestMsg>([this](CMessagePtr<ipc::IPCClientRequestMsg>& msg) {
 			auto requestKeeper = msg->getRequestKeeper();
-			if (requestKeeper->getOperationCode() == OpCode::Request && requestKeeper->getOperationID() == CSC_OpID_ScanningStatus)
+			if (requestKeeper->getOperationCode() == OpCode::Request && requestKeeper->getOperationID() == ScanningStatus::ID())
 			{
 				//_timer.start(1000, [requestKeeper, this] {
-				auto status = ScanningStatusResult::create();
-				for (int i = 1; i <= 20; i ++){
+				auto status = ScanningStatus::makeResult();
+				for (int i = 1; i <= 20; i++) {
 					std::this_thread::sleep_for(std::chrono::seconds(1));
-					status->props().set_percentage(i);
+					status->set_percentage(i);
 					requestKeeper->update(status);
 				}
-					/*auto status = ScanningStatusResult::create();
-					std::this_thread::sleep_for(std::chrono::seconds(1));
-					status->props().set_percentage(_percentage++);
-					requestKeeper->update(status);*/
-					//});
+				/*auto status = ScanningStatusResult::create();
+				std::this_thread::sleep_for(std::chrono::seconds(1));
+				status->props().set_percentage(_percentage++);
+				requestKeeper->update(status);*/
+				//});
 				requestKeeper->respond(status);
 			}
 			/*else if (requestKeeper->getOperationCode() == OpCode::Abort)
@@ -41,9 +40,7 @@ public:
 			}*/
 			});
 
-		Component::start([this] {
-			_serviceStub = ipc::LocalIPCServiceStub::createStub(SERVICE_ID_SCANNING_SERVICE);
-			});
+		_serviceStub = ipc::LocalIPCServiceStub::createStub(SERVICE_ID_SCANNING_SERVICE);
 	}
 };
 
@@ -51,5 +48,5 @@ int main()
 {
 	ipc::LocalIPCServer::instance().init(Address{ SERVER_DOMAIN_NAME, 0 });
 	ScanServiceComp comp;
-	comp.start();
+	comp.run(maf::messaging::LaunchMode::AttachToCurrentThread);
 }
